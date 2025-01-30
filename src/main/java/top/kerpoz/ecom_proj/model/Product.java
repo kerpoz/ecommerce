ee8241e9e1b6efe947eb70edb54b887b38312199
Konrad Cieślak <konrad.cieslak.kerpoz@gmail.com>
2025-01-30 12:06:49 +0100

    refactor(product): simplify getter and setter methods using Lombok

--- src/main/java/top/kerpoz/ecom_proj/model/Product.java
+++ src/main/java/top/kerpoz/ecom_proj/model/Product.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 public class Product {
 
     @Id
@@ -31,101 +35,4 @@ public class Product {
     private String imageType;
     @Lob
     private byte[] imageDate;
-
-    //for some reason Lombok won't generate setters and getters with @Getter and @Setter annotation, that's why I added them manually
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public Date getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(Date releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public boolean isProductAvailable() {
-        return productAvailable;
-    }
-
-    public void setProductAvailable(boolean productAvailable) {
-        this.productAvailable = productAvailable;
-    }
-
-    public int getStockQuantity() {
-        return stockQuantity;
-    }
-
-    public void setStockQuantity(int stockQuantity) {
-        this.stockQuantity = stockQuantity;
-    }
-
-    public String getImageName() {
-        return imageName;
-    }
-
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
-
-    public String getImageType() {
-        return imageType;
-    }
-
-    public void setImageType(String imageType) {
-        this.imageType = imageType;
-    }
-
-    public byte[] getImageDate() {
-        return imageDate;
-    }
-
-    public void setImageDate(byte[] imageDate) {
-        this.imageDate = imageDate;
-    }
 }
