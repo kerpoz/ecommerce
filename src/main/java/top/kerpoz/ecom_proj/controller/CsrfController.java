0a441ba83b6a9d1ded887c7f5b29bd3d2f7227f0
Konrad Cieślak <konrad.cieslak.kerpoz@gmail.com>
2025-02-03 00:31:39 +0100

    feat(security): add API request mapping to CsrfController for improved endpoint organization

--- src/main/java/top/kerpoz/ecom_proj/controller/CsrfController.java
+++ src/main/java/top/kerpoz/ecom_proj/controller/CsrfController.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // This controller is only enabled if the property csrf.enabled is set to true
 @ConditionalOnProperty(name = "csrf.enabled", havingValue = "true", matchIfMissing = true)
 @RestController
+@RequestMapping("/api")
 public class CsrfController {
 
     @GetMapping("/csrf-token")
