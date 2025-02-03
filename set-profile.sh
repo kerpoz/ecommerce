b48e3ea10d26ae4632e04b9675243f6abcecdfd3
Konrad Cieślak <konrad.cieslak.kerpoz@gmail.com>
2025-02-03 23:37:43 +0100

    feat(config): refactor application properties, introduce env.properties for environment variable management,

--- set-profile.sh
+++ set-profile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script is used to set the Spring profile based on the current branch, won't be necessary if we will containerize with Docker
 
 echo "Running set-profile.sh script"
 
@@ -13,4 +14,6 @@ else
   exit 1
 fi
 
+#TODO investigate why after debugging seems OK, but finally it doesn't change the env vars (at least Spring is not picking it up), use .githooks/post-checkout to automatize process
+#TODO implement Dockerfile to avoid this script
 echo "Switched to branch '$BRANCH', profile set to '$SPRING_PROFILES_ACTIVE'"
